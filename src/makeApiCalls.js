a5d366bd4df729ae3a13a3675f4db2e5c3313c1a
kartikmshah <kartik.shah@robosoftin.com>
2020-07-06 11:02:55 +0530

    added axios-retry.

--- src/makeApiCalls.js
+++ src/makeApiCalls.js
@@ -1,4 +1,12 @@
 const axios = require("axios");
+const axiosRetry = require("axios-retry");
+
+axiosRetry(axios, {
+            retryCondition: (error) => (error.code === '400' || error.code === '500'),
+            shouldResetTimeout: true,
+            retries: 5,
+            retryDelay: 1000 });
+
 const makeApiCalls = async function(method, url) {
     let data= await axios[method](url);
     return await data.data;
